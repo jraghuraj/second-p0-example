b8d9efe622f6a7ea8b2adef42eeb8d859906bb26
jraghuraj <jraghuraj@ufl.edu>
2022-03-14 15:01:48 -0400

    third upload

--- data_access_layer/customer_dal/customer_dal_imp.py
+++ data_access_layer/customer_dal/customer_dal_imp.py
@@ -1,7 +1,14 @@
 from abc import ABC
 
+from custom_exception.custom_problem import CustomProblem
 from data_access_layer.customer_dal.customer_dal_interface import CustomerDAOInterface
+from entities.customer_class import Customer
 
 
 class CustomerDAOImp(CustomerDAOInterface, ABC):
-    pass
+
+    def create_customer_entry(self, customer: Customer) -> Customer:
+        try:
+            sql = ""
+        except CustomProblem as e:
+            pass
--- tests/customer_dal_test/test_customer_dal.py
+++ tests/customer_dal_test/test_customer_dal.py
@@ -1,3 +1,4 @@
+from custom_exception.custom_problem import CustomProblem
 from data_access_layer.customer_dal.customer_dal_imp import CustomerDAOImp
 from entities.customer_class import Customer
 
@@ -13,3 +14,16 @@ def test_create_customer_entry_success():
 def test_delete_customer_entry_success():
     result = customer_dao.delete_from_customers_table_by_id(-1)
     assert result
+
+def test_create_customer_operational_error_caught():
+    try:
+        pass
+    except CustomProblem as e:
+        pass
+
+def test_delete_customer_operational_err0r_caught():
+    try:
+        customer_dao.delete_from_customers_table_by_id(0)
+        assert False
+    except CustomProblem as e:
+        pass
